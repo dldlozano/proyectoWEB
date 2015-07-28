38972d8c25dea7426c18c0b8773aaf1e6a9ed569
nukker26 <luis.nukker26@gmail.com>
2015-07-28 15:10:15 -0500

    cambio

--- SaewWS/JavaSource/com/epn/saew/controladores/PersonaControlador.java
+++ SaewWS/JavaSource/com/epn/saew/controladores/PersonaControlador.java
@@ -1,80 +1,117 @@
 package com.epn.saew.controladores;
-import java.util.List;
+
 import java.util.ArrayList;
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+
 import com.epn.saew.entidades.Persona;
+import com.epn.saew.entidades.Rol;
 import com.epn.saew.servicios.ServicioPersona;
+import com.epn.saew.servicios.ServicioRol;
 
 @ManagedBean
 @ViewScoped
-public class PersonaControlador { 
+public class PersonaControlador {
 	private Persona persona;
 	private List<Persona> personas;
 	private int idPersonaSeleccionado;
 	private boolean esActualizar;
+
+	private Rol rol;
+	private List<Rol> roles;
+	private int indexRolSeleccionado;
+
 	@EJB
 	private ServicioPersona servPersona;
-	public PersonaControlador(){
+	@EJB
+	private ServicioRol servRol;
+
+	public PersonaControlador() {
 		personas = new ArrayList<Persona>();
 	}
 
 	@PostConstruct
-	public void init(){
+	public void init() {
 		persona = new Persona();
 		personas = servPersona.buscarTodos();
+		roles = servRol.buscarTodos();
 	}
+
 	public void insertar() {
+		rol = roles.get(indexRolSeleccionado-1);
+		persona.setRol(rol);
 		servPersona.insertar(persona);
 		init();
-		limpiar();
 	}
+
 	public void actualizar() {
 		servPersona.actualizar(persona);
 		init();
-		limpiar();
 	}
+
 	public void eliminar() {
 		servPersona.eliminar(persona);
 		init();
-		limpiar();
-	}
-	public void limpiar() {
-		persona = new Persona();
 	}
 
-	//getters y setters
-	public Persona getPersona (){
+	// getters y setters
+	public Persona getPersona() {
 		return persona;
 	}
 
-	public void setPersona (Persona persona){
-		this.persona=persona;
+	public void setPersona(Persona persona) {
+		this.persona = persona;
 	}
 
-	public List<Persona> getPersonas (){
+	public List<Persona> getPersonas() {
 		return personas;
 	}
 
-	public void setPersona (List<Persona> personas){
-		this.personas=personas;
+	public void setPersona(List<Persona> personas) {
+		this.personas = personas;
 	}
 
-	public int getIdPersonaSeleccionado(){
+	public int getIdPersonaSeleccionado() {
 		return idPersonaSeleccionado;
 	}
 
-	public void setIdPersonaSeleccionado (int idPersonaSeleccionado){
-		this.idPersonaSeleccionado=idPersonaSeleccionado;
+	public void setIdPersonaSeleccionado(int idPersonaSeleccionado) {
+		this.idPersonaSeleccionado = idPersonaSeleccionado;
 	}
 
-	public boolean isesActualizar (){
+	public boolean isesActualizar() {
 		return esActualizar;
 	}
 
-	public void setEsActualizar(boolean esActualizar){
-		this.esActualizar=esActualizar;
+	public void setEsActualizar(boolean esActualizar) {
+		this.esActualizar = esActualizar;
+	}
+
+	public Rol getRol() {
+		return rol;
+	}
+
+	public void setRol(Rol rol) {
+		this.rol = rol;
+	}
+
+	public List<Rol> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Rol> roles) {
+		this.roles = roles;
+	}
+
+	public int getIndexRolSeleccionado() {
+		return indexRolSeleccionado;
+	}
+
+	public void setIndexRolSeleccionado(int indexRolSeleccionado) {
+		this.indexRolSeleccionado = indexRolSeleccionado;
 	}
 }
